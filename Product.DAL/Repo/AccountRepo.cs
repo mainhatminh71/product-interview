51058f8e550e8bdfb3923fcd118ca945c480572d
Mai Nhật Minh <nhatmoon71@gmail.com>
2025-12-09 10:28:16 +0700

    Add Repo Register/SignUp

--- Product.DAL/Repo/AccountRepo.cs
+++ Product.DAL/Repo/AccountRepo.cs
@@ -1,12 +1,43 @@
-﻿using Product.DAL.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Product.DAL.Data;
+using Product.DAL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Product.DAL.Repo
 {
-    public class AccountRepo
+    public class AccountRepo : IAccountRepo
     {
-        private ProductDbContext _context;
+        private readonly ProductDbContext _ctx;
+        public AccountRepo(ProductDbContext ctx)
+        {
+            _ctx = ctx;
+        } 
+        public async Task<User?> GetByEmailAsync(string email) 
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return null;
+
+            email = email.Trim().ToLowerInvariant();
+
+            return await _ctx.Users
+                .FirstOrDefaultAsync(u => u.Email.ToLower() == email);
+        }
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await _ctx.Users
+                .FirstOrDefaultAsync(u => u.Id == id);
+        }
+        public async Task CreateUserAsync(Entities.User user)
+        {
+            _ctx.Add(user);
+            _ctx.SaveChanges();
+        }
+        public async Task UpdateUserAsync(Entities.User user)
+        {
+            _ctx.Update(user);
+            _ctx.SaveChanges();
+        }
     }
 }
---
+++ Product.DAL/Repo/IAccountRepo.cs
@@ -0,0 +1,15 @@
+﻿using Product.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product.DAL.Repo
+{
+    public interface IAccountRepo
+    {
+        Task<User> GetByEmailAsync(string email);
+        Task<User> GetByIdAsync(int id);
+        Task CreateUserAsync(User user);
+        Task UpdateUserAsync(User user);
+    }
+}
